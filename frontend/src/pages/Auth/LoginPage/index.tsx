10a970fb131630a8fd495c50db7296726521d8ae
hydrotolerance <julzhafiz@gmail.com>
2024-05-15 23:21:12 +0800

    :white_check_mark: Login Page fix UI

--- frontend/src/pages/Auth/LoginPage/index.tsx
+++ frontend/src/pages/Auth/LoginPage/index.tsx
@@ -6,16 +6,19 @@ const LoginPage = () => {
         <div className="h-full w-full bg-slate-200/85">
             <div className=" h-[100vh] flex justify-center items-center">
                 <div className="bg-white/90 rounded shadow-lg">
+                <form action="" >
                     <div className="w-96 h-64 flex justify-center items-center flex-col">
                         <h2>Login</h2>
-                        <h2 >Username</h2>
-                        <input type="text" className="border-2"/>
+                        <h2 >Email</h2>
+                        <input type="text" value="email" className="border-2"/>
                         <h2>Password</h2>
-                        <input type="text" className="border-2"/>
+                        <input type="text" value="password" className="border-2"/>
+                        <button type="submit">Submit</button>
                         <div className="mt-10">
                             <Link to={'/register'}>Dont have an account</Link>
                         </div>
                     </div>
+                </form>
                 </div>
             </div>
         </div>
